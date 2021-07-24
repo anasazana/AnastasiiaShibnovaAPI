7d9f8c9153ae62b6236c5923a219a309350596d6
Anastasiia Shibanova <anastasiia_shibanova@epam.com>
2021-07-24 14:40:59 +0300

    key and token are now retrieved from system properties

--- src/test/java/ru/training/at/hw10/constants/CommonValues.java
+++ src/test/java/ru/training/at/hw10/constants/CommonValues.java
@@ -2,8 +2,8 @@ package ru.training.at.hw10.constants;
 
 public class CommonValues {
     public static final String BASE_URL = "https://trello.com";
-    public static final String KEY = "7baaca8bc3fdf99798502d9593ce43e3";
-    public static final String TOKEN = "684ab47bfbed474a8c82835f6d6eff06b57e4343a1ef2ffd5a66cd0ba31b19ae";
+    public static final String KEY = System.getProperty("trello.key");
+    public static final String TOKEN = System.getProperty("trello.token");
     public static final int MAX_BOARDS_AVAILABLE_NON_PREMIUM = 10;
     public static final String DEFAULT_TEST_BOARD_NAME = "TestBoard";
     public static final String MEMBER_CREATOR_ID = "5d908cb5f125d10bc8b518b2";
